e7dae66a352b7c5368a854ea959418f67cca83f8
Marshal Wu <marshal.wu@gmail.com>
2022-03-08 18:59:41 +0800

    update

--- packages/simple-nlp/src/index.ts
+++ packages/simple-nlp/src/index.ts
@@ -1,6 +1,11 @@
 import natural from "natural";
 
-export function parseSentences(content: string) {
-  var tokenizer = new natural.SentenceTokenizer();
+// export function parseSentences(content: string) {
+//   var tokenizer = new natural.SentenceTokenizer();
+//   return tokenizer.tokenize(content);
+// }
+
+export const parseSentences = function (content: string) {
+  const tokenizer = new natural.SentenceTokenizer();
   return tokenizer.tokenize(content);
-}
+};
