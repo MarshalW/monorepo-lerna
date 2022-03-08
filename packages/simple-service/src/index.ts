52100bb4a2e0e877da5ea6b2b0844a3aaa69cbea
Marshal Wu <marshal.wu@gmail.com>
2022-03-08 18:31:10 +0800

    update

--- packages/simple-service/src/index.ts
+++ packages/simple-service/src/index.ts
@@ -1,3 +1,24 @@
 import { parseSentences } from "mw-simple-nlp";
+import Fastify from "fastify";
 
-console.log("simple service");
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, reply) => {
+  reply.send({ hello: "world" });
+});
+
+fastify.post("/", (request, reply) => {
+  console.log(request.body);
+  let content: any = request.body;
+  let text: string = content.content;
+
+  reply.send({ lines: parseSentences(text) });
+});
+
+// Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+  // Server is now listening on ${address}
+});
